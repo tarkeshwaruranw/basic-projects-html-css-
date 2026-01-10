fff2fd1ba14415ebcf945ae7a8a408bed88f4c7c
Tarkeshwar Uranw <tarkeshwaruranw111@gmail.com>
2026-01-10 23:51:38 +0530

    card1 added

--- DOM/random-box/script.js
+++ DOM/random-box/script.js
@@ -1,6 +1,21 @@
-const box = document.querySelector('.box');
+const main = document.querySelector('main');
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
-  console.log("clicked")
+  var c1 = Math.floor(Math.random()*256);
+  var c2 = Math.floor(Math.random()*256);
+  var c3 = Math.floor(Math.random()*256);
+  var x = Math.floor(Math.random()*100);
+  var y = Math.floor(Math.random()*100);
+
+  var div = document.createElement('div');
+  div.style.height = "65px";
+  div.style.width = "65px";
+  div.style.position = "absolute";
+  div.style.left = x+"%"
+  div.style.top = y+"%"
+  div.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
+  // div.style.backgroundColor = 'red';
+  main.appendChild(div);
+  console.log(div)
 })
